e2e2d0a1fb61366b3fdece596abd1b40c645d9f4
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2024-03-19 00:29:30 +0900

    Ver.0.2.0
    ChangingKey.py
    - 任意のキーに転調するクラス

--- ChangingTest.py
+++ ChangingTest.py
@@ -1,5 +1,4 @@
 import convert.ChangingKey as change
 
 datasets = "data/JazzMidi/"
-pitches = change.ChangingKey(datasets + "55Dive.mid").set_key()
-print(pitches)
+pitches = change.ChangingKey(datasets + "55Dive.mid")
--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import convert.ChangingKey as changeKey
 import numpy as np
 datasets = "data/JazzMidi/"
 
-midi_data = changeKey.ChangingKey(datasets + "55Dive.mid").convert()
 
-converter = midiNum.MidiConvertToNumPy(datasets + "55Dive.mid", data=midi_data)
+converter = midiNum.MidiConvertToNumPy(datasets + "55Dive.mid")
 numpy_data = converter.convert()
+change = changeKey.ChangingKey(datasets + "55Dive.mid", numpy_data)
+change.set_convert_key("C")
+numpy_data = change.convert()
+
 np.set_printoptions(precision=2, suppress=True)
 print(numpy_data[0])
+
